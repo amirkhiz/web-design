9bc9ddf79a20dca148ad58c0c53b66340e8e8dd7
Siavash Habil <amirkhiz@gmail.com>
2019-11-05 13:42:08 +0300

    - Refactoring   - Change landing.html to index.html index is a intro file name   - Refactor css file (remove units for zero values)   - Rename id and classes to meaningful words   - Use button tag for tags does not link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-document.getElementById('butto0n').addEventListener('click', function () {
+document.getElementById('btn-open').addEventListener('click', function () {
     document.querySelector('.bg-model').style.display = 'flex';
 });
 
@@ -7,8 +7,8 @@ document.getElementById('close').addEventListener('click', function () {
 });
 
 document.getElementById('join').addEventListener('click', function () {
-    document.querySelector('.modeljoin').style.display = 'flex';
+    document.querySelector('.model-join').style.display = 'flex';
 });
-document.getElementById('tabcloser').addEventListener('click', function () {
-    document.querySelector('.modeljoin').style.display = 'none';
+document.getElementById('tab-closer').addEventListener('click', function () {
+    document.querySelector('.model-join').style.display = 'none';
 });
